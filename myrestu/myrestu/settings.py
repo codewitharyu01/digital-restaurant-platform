184fe85276ea7aba023396c8f34c4c6d8d93eefb
Aryan RD <officialboyv3@gmail.com>
2026-01-03 00:10:27 +0530

    connected PostSQL database

--- myrestu/myrestu/settings.py
+++ myrestu/myrestu/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,11 +79,19 @@ WSGI_APPLICATION = 'myrestu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+ #  
+  #  'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+   # }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://myrestu_db_user:Pz0GY5NryDpqVrn4K8oSwb9oG4hs9DWI@dpg-d5c0ro2li9vc73c2ksqg-a/myrestu_db',
+        conn_max_age=600
+    )
 }
 
 
