3720ca4041efde898082bee89d50d89e87f32bbc
Aryan RD <officialboyv3@gmail.com>
2026-01-03 16:28:11 +0530

    add admin and register

--- myrestu/fakerestu/admin.py
+++ myrestu/fakerestu/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from .models import Repass,FoodDiliver,BookingTable,FeedbackForm
 
 # Register your models here.
 
+admin.register(Repass)
+admin.register(FoodDiliver)
+admin.register(BookingTable)
+admin.register(FeedbackForm)
 
